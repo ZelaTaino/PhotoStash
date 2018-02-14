a9506625a0d6198fb1b0a0e7d9b8de9ac491c906
Ifeoma Ufondu <Ifeoma@ifeomas-mbp.dhcp.wustl.edu>
2018-02-14 17:25:27 -0600

    Trying to commit

---
+++ PhotoStash/ViewContoller.swift
@@ -0,0 +1,19 @@
+//
+//  ViewContoller.swift
+//  PhotoStash
+//
+//  Created by Ifeoma Ufondu on 2/13/18.
+//  Copyright © 2018 photostash. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
